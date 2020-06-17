2392178b6c9fe047ab5cc1f7b11a70e6d7dfeb84
alekoxo <aleko@incendiaryblue.com>
2020-06-17 15:35:42 -0700

    added new styles sheet to component

---
+++ src/components/heading/heading.js
@@ -0,0 +1,12 @@
+import './heading.scss';
+
+class heading {
+    render() {
+        const h1 = document.createElement('h1');
+        const body = document.querySelector('body');
+        h1.innerHTML = 'Webpack is Awesome';
+        body.appendChild(h1);
+    }
+}
+
+export default heading;
