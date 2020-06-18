80851005857e1fa7df433fa6502f7ea7819d3d4a
alekoxo <alex.kasongolenge@yahoo.com>
2020-06-18 15:31:25 -0700

    handlebars loader is now working

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,8 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin  = require('clean-webpack-plugin');
+const { CleanWebpackPlugin }  = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const template = require("./file.handlebars");
 
 module.exports = {
     /**
