ef3fcc6d4f5570d1a822b0a70342e2873ba3882c
alekoxo <alex.kasongolenge@yahoo.com>
2020-06-21 16:43:20 -0700

    fontawesome now working

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,6 +1,8 @@
 /**
  * import dependencies - else they won't work
  */
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 import HelloWorldButton from './components/hello-world-button/hello-world-button.js'
 import Heading from './components/heading/heading.js'
@@ -8,6 +10,9 @@ import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css'; // webpack does not bundle bootstrap when used this way
 import './index.scss';
 // import React from 'react'; 
+
+library.add(faSpinner);
+dom.watch();
 
 const heading = new Heading();
 heading.render();
